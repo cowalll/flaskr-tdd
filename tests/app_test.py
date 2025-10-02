3ff2d8846df908dd9d402214ad4d466ae268b2de
cowalll <carl.j.ancheta@gmail.com>
2025-10-01 22:26:41 -0400

    Linting and Code Formatting

--- tests/app_test.py
+++ tests/app_test.py
@@ -7,6 +7,7 @@ from project.app import app, db
 
 TEST_DB = "test.db"
 
+
 @pytest.fixture
 def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,6 +19,7 @@ def client():
         db.create_all()  # setup
         yield app.test_client()  # tests run here
         db.drop_all()  # teardown
+
 
 def login(client, username, password):
     """Login helper function"""
@@ -74,6 +76,7 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_search(client):
     """Ensure that user can search messages"""
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
@@ -82,10 +85,11 @@ def test_search(client):
         data=dict(title="Test title", text="Test text"),
         follow_redirects=True,
     )
-    rv = client.get('/search/?query=Test')
+    rv = client.get("/search/?query=Test")
 
     assert b"Test title" in rv.data
     assert b"Test text" in rv.data
+
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
